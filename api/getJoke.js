5f73dc8f062e064a7b33fa3fcfe18720278e5ad0
Mr. Undefined <service.b@blog.guidefather.in>
2020-07-11 16:35:27 +0530

    v3

--- api/getJoke.js
+++ api/getJoke.js
@@ -1,8 +1,26 @@
+var giveMeAJoke = require('give-me-a-joke');
+
+
 exports.handler = async (event,context,cb) => {
-    return {
+  
+  function joker() {
+    return new Promise(function (resolve, reject) {
+      giveMeAJoke.getRandomDadJoke(function (joke) {
+        resolve(joke)
+
+      });
+    })
+  }
+
+  //getting random joke
+  let joke = await joker()
+   return cb(null, {
+    statusCode: 200,
+    body: JSON.stringify({
       statusCode: 200,
       message:"success",
-      body: "Here is a joke",
-    }
-  
-  }
+      body: joke,
+    })
+  })
+}
+
